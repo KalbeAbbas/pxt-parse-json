8bcddc8ef44ade9fbaf7bc458c6c2f12497710a4
KalbeAbbas <kalbeabbas142@gmail.com>
2020-09-07 19:44:10 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 //% color=190 weight=100 icon="\uf7ea" block="JSON blocks"
 namespace parse_json
 {
-
+    //% blockId=device_show_number
+    //% block="show|number %v"
+    export function showNumber(v: number, interval: number = 150): void
+    { }
 }
