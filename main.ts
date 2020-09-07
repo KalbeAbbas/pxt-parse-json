f71e14b2e4e94574bccfd052d9a572eb2a35a1e9
KalbeAbbas <kalbeabbas142@gmail.com>
2020-09-07 19:55:53 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 //% color=190 weight=100 icon="\uf1c0" block="XinaBox JSON"
 namespace xinabox_json
 {
+    //% blockId=parse_json
+    //% block="XinaBox parse json %str"
+    export function parse_json(str: string)
+    {
 
+    }
 }
