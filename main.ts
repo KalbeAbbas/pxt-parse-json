f19ac2fe8f13f732ebec592335b6349cda8d8a3b
KalbeAbbas <kalbeabbas142@gmail.com>
2020-09-08 12:01:31 +0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
-//% color=190 weight=100 icon="\uf1c0" block="XinaBox JSON"
 namespace xinabox_json
 {
     //% blockId=parse_json
     //% block="XinaBox parse json %str"
     export function parse_json(str: string)
     {
+        let first_curly_bracket_find = str.indexOf("{",0)
+        let id_first_quotation_find = str.indexOf("\"", first_curly_bracket_find)
+        let id_second_quotation_find = str.indexOf("\"", id_first_quotation_find)
+        let id: string = str.substr(id_first_quotation_find, id_second_quotation_find - id_first_quotation_find)
 
+        
     }
 }
